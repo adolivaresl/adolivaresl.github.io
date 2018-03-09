858f05927bc06ce26c9d1379607effbbf9163011
adolivaresl <domioli@gmail.com>
2018-03-08 23:43:31 -0300

    transicion de scroll listo prueba para ghpages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-jQuery(document).ready(function(){
+$(document).ready(function(){
     $(".oculto").hide();              
-      $(".inf").click(function(){
+      $(".inf").hover(function(){
             var nodo = $(this).attr("href");  
    
             if ($(nodo).is(":visible")){
@@ -11,5 +11,28 @@ jQuery(document).ready(function(){
           $(nodo).fadeToggle("fast");
           return false;
             }
+      });
+
+        // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+    
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+       
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
       });
   }); 
